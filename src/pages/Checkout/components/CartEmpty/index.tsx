aa7d155f9e113e37287e15e61a99d7159092fef0
Leandro Campos <camposweb@gmail.com>
2023-04-24 16:07:11 -0300

    Edit Component CartEmpty

--- src/pages/Checkout/components/CartEmpty/index.tsx
+++ src/pages/Checkout/components/CartEmpty/index.tsx
@@ -1,10 +1,19 @@
-import { TextRegularL } from '../../../../components/Fonts'
-import { CartEmptyContainer } from './styles'
+import { NavLink } from 'react-router-dom'
+import { TitleM } from '../../../../components/Fonts'
+import { CartEmptyContainer, CartEmptyContent } from './styles'
 
 export function CartEmpty() {
   return (
     <CartEmptyContainer>
-      <TextRegularL>Não existe item no carrinho</TextRegularL>
+      <CartEmptyContent>
+        <div>
+          {/* <TextRegularL>O carrinho está vazio</TextRegularL> */}
+          <TitleM>O carrinho está vazio</TitleM>
+          <NavLink to="/" className="link">
+            voltar para home
+          </NavLink>
+        </div>
+      </CartEmptyContent>
     </CartEmptyContainer>
   )
 }
--- src/pages/Checkout/components/CartEmpty/styles.ts
+++ src/pages/Checkout/components/CartEmpty/styles.ts
@@ -3,4 +3,52 @@ import styled from 'styled-components'
 export const CartEmptyContainer = styled.main`
   width: 100%;
   padding: 0 10rem;
+
+  @media (max-width: 768px) {
+    padding: 0 3rem;
+  }
+`
+
+export const CartEmptyContent = styled.div`
+  display: flex;
+  height: 50vh;
+  justify-content: center;
+  border-radius: 0.375rem;
+  background: ${({ theme }) => theme.colors.card};
+
+  div {
+    display: flex;
+    gap: 0.5rem;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    a {
+      text-decoration: none;
+
+      font-family: 'Roboto', sans-serif;
+      font-style: normal;
+      font-weight: 700;
+      font-size: 0.875rem;
+      line-height: 1.375rem;
+      text-transform: uppercase;
+      //padding: 0.75rem 2.813rem;
+      padding: 0.5rem 1rem;
+      border-radius: 0.375rem;
+      outline: none;
+      //cursor: pointer;
+      border: 2px solid ${({ theme }) => theme.colors.yellowDark};
+      text-decoration: none;
+      transition: 0.2s;
+      /* box-shadow: 0 0 0 3px ${({ theme }) => theme.colors.yellowDark}; */
+      color: ${({ theme }) => theme.colors.subtitle};
+      //background: ${({ theme }) => theme.colors.yellow};
+      background: transparent;
+
+      &:hover {
+        background: ${({ theme }) => theme.colors.yellowDark};
+        color: ${({ theme }) => theme.colors.white};
+      }
+    }
+  }
 `
